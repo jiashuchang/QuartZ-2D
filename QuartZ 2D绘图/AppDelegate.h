25aa64a249ccb18806ab1ae627af9161f261843c
TianLi <tianli@TianLideiMac.local>
2018-09-19 15:51:11 +0800

    Initial Commit

---
+++ QuartZ 2D绘图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuartZ 2D绘图
+//
+//  Created by TianLi on 2018/9/19.
+//  Copyright © 2018年 TianLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuartZ 2D绘图/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuartZ 2D绘图
+//
+//  Created by TianLi on 2018/9/19.
+//  Copyright © 2018年 TianLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
